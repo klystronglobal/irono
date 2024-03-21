f2544b031d484cc20822aa8804c8253215bb1291
arv-aravind <aravindu28@gmail.com>
2024-03-21 13:53:41 +0530

    [UPDATE] manifest version name change

--- kg_irono_mobile_api/__manifest__.py
+++ kg_irono_mobile_api/__manifest__.py
@@ -3,7 +3,7 @@
     "name": "KG IRONO Mobile API",
     "summary": """KG Mobile API""",
     "category": "Website",
-    "version": "16.0.0",
+    "version": "17.0.0",
     "author": "Klystron Global",
     "maintainer": "Aravind",
     "license": "Other proprietary",
